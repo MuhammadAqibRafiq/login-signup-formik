69d5fe5f9a9e74a3135e10b308f1dd998124defa
Muhammad Aqib Rafiq <aqibrafiq001@outlook.com>
2021-06-30 23:30:53 +0500

    updated Navbar

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,21 +1,42 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { Navbar , Nav } from 'react-bootstrap'
 
 const NavBar = () => {
+
     return (
-        <nav class="navbar navbar-light"   style={{background:"#e3f2fd"}}>
-            <Link to="/home"><a class="navbar-brand">Navbar</a></Link>
-            <form class="form-inline">
-                <Link to="/login"><button class="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button></Link>
-                <div>
-                <Link to="/signup" class="btn btn-outline-success my-2 my-sm-0">SignUp</Link> 
-                </div> 
+        // <nav className="navbar navbar-dark"   style={{background:"#708090"}} >
+        //     <Link to="/home" variant="dark"><a className="navbar-brand">Navbar</a></Link>
+            
+        //     <form className="form-inline">
+        //           <Nav.Link href="#home"  style={{background:"#708090"}}>Home</Nav.Link>
+
+        //         <Link to="/login"><button className="btn btn-outline-success my-2 my-sm-0" type="submit">Login</button></Link>
+        //         <div>
+        //         <Link to="/signup" class="btn btn-outline-success my-2 my-sm-0">SignUp</Link> 
+        //         </div> 
 
                
-            </form>
-        </nav>
+        //     </form>
+        // </nav>
 
-            )
+    //     <Navbar style={{background:"#708090"}} variant="dark">
+    // <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    // <Nav className="ml-auto">
+    //   <Nav.Link href="#home" className='left'>Home</Nav.Link>
+    //   <Nav.Link href="#features">LOGIN</Nav.Link>
+    //   <Nav.Link href="#pricing">Pricing</Nav.Link>
+    // </Nav>
+
+    <Navbar bg="dark" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="ml-auto">
+      <Nav.Link href="/login" >LogIn</Nav.Link>
+      <Nav.Link href="/signup" >SignUp</Nav.Link>
+    </Nav>
+    </Navbar>
+
+            );
 }
 
-export default NavBar
+export default NavBar;
